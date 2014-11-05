af5a8b5e597549e231565e8bb88480606564dc91
Robert Agthe <robert@scriptshit.de>
2014-11-05 18:16:21 +0100

    soundcloud added, refactoring

--- index.js
+++ index.js
@@ -53,25 +53,65 @@ parseVideoSite = function(html, url) {
   return data;
 };
 
+// parse a known sound portal
+parseSoundcloud = function(response, url) {
+  parsedBody = JSON.parse(response.body);
+  return {
+    title: parsedBody.title,
+    description: parsedBody.description,
+    thumbnail: parsedBody.thumbnail_url,
+    type: 'sound',
+    url: url,
+    embed: parsedBody.html,
+    full_response: parsedBody
+  };
+};
+
 // get the site, parse it and return what we found
 exports.scrape = function(req, res) {
-  return request(req.body.url, function(error, response, html) {
+  var parsedObj;
 
-    var parsedObj;
-
-    // parse different sites and media different ;)
-    if (req.body.url.indexOf('.jpg') !== -1 || req.body.url.indexOf('.jpeg') !== -1 || req.body.url.indexOf('.gif') !== -1 || req.body.url.indexOf('.png') !== -1) {
+  // parse different sites and media different ;)
+  if (req.body.url.indexOf('.jpg') !== -1 || req.body.url.indexOf('.jpeg') !== -1 || req.body.url.indexOf('.gif') !== -1 || req.body.url.indexOf('.png') !== -1) {
+    request(req.body.url, function(error, response, html){
       parsedObj = parseImage(html, req.body.url);
-    } else if (req.body.url.indexOf('flickr.com/') !== -1 || req.body.url.indexOf('imgur.com/') !== -1) {
+ 
+      // send header and response
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(parsedObj));
+    });
+  } else if (req.body.url.indexOf('flickr.com/') !== -1 || req.body.url.indexOf('imgur.com/') !== -1) {
+    request(req.body.url, function(error, response, html){
       parsedObj = parseImageSite(html, req.body.url);
-    } else if (req.body.url.indexOf('vimeo.com/') !== -1 || req.body.url.indexOf('youtube.com/') !== -1){
+ 
+      // send header and response
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(parsedObj));
+    });
+  } else if (req.body.url.indexOf('vimeo.com/') !== -1 || req.body.url.indexOf('youtube.com/') !== -1){
+    request(req.body.url, function(error, response, html){
       parsedObj = parseVideoSite(html, req.body.url);
-    } else {
+ 
+      // send header and response
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(parsedObj));
+    });
+  } else if (req.body.url.indexOf('soundcloud.com/') !== -1){
+    request('http://soundcloud.com/oembed?format=json&amp;url='+req.body.url, function(error, response, html){
+      parsedObj = parseSoundcloud(response, req.body.url);
+ 
+      // send header and response
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(parsedObj));
+    });
+  } else {
+    request(req.body.url, function(error, response, html){
       parsedObj = parseRegularWebsite(html, req.body.url);
-    }
-
-    // send header and response
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(parsedObj));
-  });
+      console.log(parsedObj);
+ 
+      // send header and response
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(parsedObj));
+    });
+  }
 };
